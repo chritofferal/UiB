5d8e5b67e06c8cf0c5038575a79edb89c543fb92
Christoffer Lorenzen <christofferal@gmail.com>
2014-03-13 19:39:59 +0100

    Started monster hirachy classes

--- info132 - oblig3/src/models/Monster.java
+++ info132 - oblig3/src/models/Monster.java
@@ -2,9 +2,11 @@ package models;
 
 public class Monster {
 
-	private int health, minDmg; 
+	private int health, minDmg, level; 
+	private double dmgMultiplyer; 
 	private String name; 
 	final int MIN_DMG = 20; 
+	
 	
 	
 	
@@ -12,13 +14,62 @@ public class Monster {
 	 * @param health health of the new monster
 	 * @param name name of the new monster
 	 */
-	public Monster(String name, int health) {		
+	public Monster(String name, int health, int level) {		
 		setHealth(health);
 		setMinDmg(MIN_DMG);
 		setName(name);
+		setLevel(level);
+	}
+	
+	public int attack(){
+		if (miss() != true){
+			System.out.println(MIN_DMG * dmgMultiplyer);
+			return (int) (MIN_DMG * dmgMultiplyer); 
+		} else return -1; 
+	}
+	
+	private boolean miss(){
+		int random = (int)(Math.random()*10+1);
+	
+		if (random == 10){
+			return true;
+		} else return false; 
 	}
 	
 	
+	
+	/**
+	 * @return the dmgMultiplyer
+	 */
+	public double getDmgMultiplyer() {
+		return dmgMultiplyer;
+	}
+
+
+	/**
+	 * @param dmgMultiplyer the dmgMultiplyer to set
+	 */
+	public void setDmgMultiplyer(double dmgMultiplyer) {
+		this.dmgMultiplyer = dmgMultiplyer;
+	}
+	
+	
+	/**
+	 * @return the level
+	 */
+	public int getLevel() {
+		return level;
+	}
+
+
+	/**
+	 * @param level the level to set
+	 */
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
 	/**
 	 * @return the health
 	 */
@@ -56,4 +107,11 @@ public class Monster {
 		this.name = name;
 	}
 	
+	
+	public static void main(String[] args) {
+		
+		Monster monster = new Monster("Goblinator", 100	, 1);
+		monster.attack();
+		
+	}
 }
