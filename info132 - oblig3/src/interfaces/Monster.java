60c7ac8b5f941886a784892a9d81cca8bad9eef7
Christoffer Lorenzen <christofferal@gmail.com>
2014-03-16 22:08:34 +0100

    asd

---
+++ info132 - oblig3/src/interfaces/Monster.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+public interface Monster {
+
+	/**
+	 * Attacks the player
+	 */
+	public void attack();
+
+	/**
+	 * Checks if the monster is dead
+	 */
+	
+	public boolean dead(); 
+	
+	
+	
+}
